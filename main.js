bfa1b306cfa5d459263f11b3dca009988396962c
mistellaa <83026698+mistellaa@users.noreply.github.com>
2021-05-25 23:03:04 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+/* let a = +prompt("Input number");
+alert(a**2);*/
+/*let m = +prompt("Input one number");
+let n = +prompt("Input another number");
+console.log("(" + m + " + " + n + ")" + "/" + "2" + " = " +((m+n)/2));*/
+/*let p = +prompt("Input the length of a square");
+alert("The square of a square is " + p*p);*/
+/*const mile = 0.621371;
+let c = +prompt("Input number");
+console.log(c + " kilometers are " + c*mile + " miles"); */
+a = +prompt("Input first number");
+b = +prompt("Input second number");
+alert((a + " + " + b + " = " + (a + b)) + "\n" + (a + " - " + b + " = " + (a - b)) + "\n" + (a + " * " + b + " = " + (a * b)) + "\n" + (a + " / " + b + " = " + (a / b)))
